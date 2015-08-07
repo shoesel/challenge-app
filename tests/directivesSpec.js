8d2bd6ed3d70b44424605364f8aaa010a221fe92
shoesel <me@hoesel.de>
2015-08-07 22:15:25 +0200

    US00 Added browser-geolocation to initialize map, added tests

--- tests/directivesSpec.js
+++ tests/directivesSpec.js
@@ -2,12 +2,18 @@ describe("mapContainer", function(){
 	var platform, map;
 
 	beforeEach(function(){
-		window.H = {service:{Platform: function(){}}, Map: function(){}};
 		platform = {createDefaultLayers: function(){return {normal:{map: {}}};}};
 		spyOn(window.H.service, "Platform").andReturn(platform);
-		map = {};
+		
+		map = {setCenter: function(){}};
 		spyOn(window.H, "Map").andReturn(map);
+		spyOn(map, "setCenter");
 	});
+
+	afterEach(inject(function(mapProxy){
+		delete mapProxy.map;
+		delete mapProxy.platform;
+	}));
 
 	beforeEach(module("chApp"));
 
@@ -31,5 +37,58 @@ describe("mapContainer", function(){
 		$rootScope.$digest();
 		expect(window.H.Map).toHaveBeenCalled();
 		expect(mapProxy.map).toBe(map);
+		expect(map.setCenter).toHaveBeenCalledWith(jasmine.any(Object));
 	}));
+
+	describe("with geolocation", function(){
+		var pos = {
+			coords: {
+					longitude: 10,
+					latitude: 20
+				}
+			},
+			group = {};
+
+		beforeEach(function(){
+			navigator.geolocation = {getCurrentPosition: function(){}};
+			spyOn(navigator.geolocation, "getCurrentPosition").andCallFake(function(cb){
+				spyOn(map, "addObject");
+				cb(pos);
+			});
+
+			spyOn(window.H.map, "Marker");
+			spyOn(window.H.map, "Group").andReturn(group);
+
+			map.addObject = function(){};
+			map.addEventListener = function(){};
+			map.removeEventListener = function(){};
+			map.setZoom = function(){};
+			spyOn(map, "setZoom");
+			spyOn(map, "removeEventListener");
+		});
+
+		it("should add a marker on location", inject(function($rootScope, $compile, mapProxy){
+			var element = $compile('<div map-container></div>')($rootScope);
+			$rootScope.$digest();
+			expect(navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
+			expect(window.H.map.Marker).toHaveBeenCalledWith({lng: 10, lat: 20});
+			expect(window.H.map.Group).toHaveBeenCalledWith(jasmine.any(Object));
+			expect(map.addObject).toHaveBeenCalled();
+		}));
+
+		it("should queue centering and zooming to location", inject(function($rootScope, $compile, mapProxy){
+			var eventHandler;
+			spyOn(map, "addEventListener").andCallFake(function(name, cb){
+				eventHandler = cb;
+			});
+			var element = $compile('<div map-container></div>')($rootScope);
+			$rootScope.$digest();
+			expect(map.addEventListener).toHaveBeenCalledWith("mapviewchangeend", jasmine.any(Function));
+			expect(map.setCenter).toHaveBeenCalledWith({lng: 10, lat:20}, true);
+			expect(map.setZoom).not.toHaveBeenCalled();
+			eventHandler.apply(map);
+			expect(map.setZoom).toHaveBeenCalled();
+			expect(map.removeEventListener).toHaveBeenCalled();
+		}));
+	});
 });
