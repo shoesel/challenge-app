bd39b11fdd08f257bc668e8b26493a6c60b78f23
shoesel <me@hoesel.de>
2015-08-07 22:15:25 +0200

    US00 Added browser-geolocation for initial map, added tests

--- app/directives.js
+++ app/directives.js
@@ -4,16 +4,46 @@ angular
 	return {
 		link: function(scope, elem, attr, ctrl){
 			if(!mapProxy.platform){
-				mapProxy.platform = new H.service.Platform({
-					'app_id': mapProxy.app_id,
-	  				'app_code': mapProxy.app_code
+				try{
+					mapProxy.platform = new H.service.Platform({
+						'app_id': mapProxy.app_id,
+		  				'app_code': mapProxy.app_code
+					});
+				} catch(ex){}
+				if(!mapProxy.platform){
+					elem.append("Error during initialization!");
+					return;
+				}
+			}
+			var defaultLayers = mapProxy.platform.createDefaultLayers(),
+				startPos = {
+					lng: 13.4,
+					lat: 52.51
+				};
+			mapProxy.map = new H.Map(elem[0], defaultLayers.normal.map, {zoom: 10});
+			mapProxy.map.setCenter(startPos);
+			if(navigator.geolocation){
+				navigator.geolocation.getCurrentPosition(function(pos){
+					var myPos = {
+							lng: pos.coords.longitude,
+							lat: pos.coords.latitude,
+						},
+						marker = new H.map.Marker(myPos),
+						group = new H.map.Group({
+							objects: [marker]
+						});
+
+					mapProxy.map.addObject(group);
+					
+					mapProxy.map.setCenter(myPos, true);
+
+					mapProxy.map.addEventListener("mapviewchangeend", function(){
+						this.setZoom(15, true);
+						this.removeEventListener("mapviewchangeend");
+					})
+					;
 				});
 			}
-			var defaultLayers = mapProxy.platform.createDefaultLayers();
-			mapProxy.map = new H.Map(elem[0], defaultLayers.normal.map, {
-				zoom: 10,
-				center: { lng: 13.4, lat: 52.51 }
-			});
 		}
 	};
 }])
---
+++ test-libs/mocks.js
@@ -0,0 +1,12 @@
+(function(window){
+	window.H = {
+		service: {
+			Platform: function(){}
+		},
+		Map: function(){},
+		map: {
+			Group: function(){},
+			Marker: function(){}
+		}
+	};
+})(window);
