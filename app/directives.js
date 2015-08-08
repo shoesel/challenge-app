5be32481202bb3c4a742206cb95ac179db950bcb
shoesel <me@hoesel.de>
2015-08-08 22:39:30 +0200

    US04 Minor tweaks

--- app/directives.js
+++ app/directives.js
@@ -23,7 +23,7 @@ angular
 			mapProxy.startPos = startPos;
 			mapProxy.zoom = 15;
 			mapProxy.map = new H.Map(elem[0], defaultLayers.normal.map, {zoom: 10});
-			mapProxy.map.setCenter(startPos);
+			mapProxy.map.setCenter(startPos, true);
 			if(navigator.geolocation){
 				navigator.geolocation.getCurrentPosition(function(pos){
 					MarkerFactory.placeMarker({
